524b2061784aabde84c3c4a4e95650019768255e
Anurag Satapathy <anurag.satapathy401@gmail.com>
2025-07-07 08:24:06 +0530

    Thymeleaf auth

--- src/main/java/com/epharm/config/AppConfig.java
+++ src/main/java/com/epharm/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -69,5 +70,13 @@ public class AppConfig {
     	
     	
     }
+    
+    
+	/*
+	 * protected void configure(HttpSecurity http) throws Exception {
+	 * 
+	 * 
+	 * }
+	 */
     
 }
